13e40a7d1aa2b6af0a82fdafd306050911d195a7
Erin Gibbs <three_rin@outlook.com>
2020-01-30 19:11:04 -0600

    created a websocket server resolving issue 5

--- app.js
+++ app.js
@@ -3,12 +3,17 @@
 //require is like import, you can find in package.json
 const express = require('express');
 const http = require('http');
-
+const WebSocket = require('ws');
 const app = express();
 //use is a middleware that allows us to serve up all the contents in static public as static resources
 app.use(express.static('public'));
-
+//http is stateless, like sending a letter through snailmail, http is out of your hands once you send a request; no way to add additional data
 const server = http.createServer(app);
+//websockets is a new protocol - its like a tin cans with a string between them - the connection stays open and you can send data back and forth
+const wss = new WebSocket.Server({server:server});
+wss.on('connection',function(we,request){
+    console.log('Connected!')
+});
 
 server.listen(8080,function(){
     console.log('App listening on port 8080');
